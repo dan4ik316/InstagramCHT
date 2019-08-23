ace28dfa48b2f6eedf2803fac4f841177587f7f1
dan4ik316 <Dan4ik@mac-mini-alex.local>
2019-08-23 15:34:05 +0300

    Customise screens and project

--- InstagramCHT/AppDelegate.swift
+++ InstagramCHT/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //Change color of tab bar's items
+        UITabBar.appearance().tintColor = UIColor.black
         return true
     }
 
