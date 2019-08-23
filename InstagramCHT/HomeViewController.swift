5473573e6ff0dc2ffab4405615364c4a7a505f59
dan4ik316 <Dan4ik@mac-mini-alex.local>
2019-08-23 12:38:30 +0300

    All views have links

---
+++ InstagramCHT/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewController.swift
+//  InstagramCHT
+//
+//  Created by Alex Kravchook on 23.08.2019.
+//  Copyright © 2019 DanKravchook. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Change color of view
+        view.backgroundColor = UIColor.black
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
