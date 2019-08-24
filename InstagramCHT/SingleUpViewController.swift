c600fce323b50f9459752cac6f5cd44eda07cd35
dan4ik316 <Dan4ik@mac-mini-alex.local>
2019-08-24 22:17:56 +0300

    Integrate profile image photo upload to firebase storage

--- InstagramCHT/SingleUpViewController.swift
+++ InstagramCHT/SingleUpViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import FirebaseAuth
 import Firebase
 import FirebaseDatabase
+import FirebaseStorage
 
 class SingleUpViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
@@ -19,6 +20,8 @@ class SingleUpViewController: UIViewController {
     @IBOutlet weak var profileImage: UIImageView!
     
     var ref: DatabaseReference!
+    
+    var selectedImage: UIImage?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,8 +64,18 @@ class SingleUpViewController: UIViewController {
         
         profileImage.layer.cornerRadius = 39
         profileImage.clipsToBounds = true
-
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(SingleUpViewController.handleSelectProfileImageView))
+        profileImage.addGestureRecognizer(tapGesture)
+        profileImage.isUserInteractionEnabled = true
         // Do any additional setup after loading the view.
+    }
+    
+    func handleSelectProfileImageView(){
+        print("Tapped!") //оставлю, ибо залипать на это в консоле приятно
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
     }
 
     @IBAction func dismiss_onClick(_ sender: Any) {
@@ -75,13 +88,41 @@ class SingleUpViewController: UIViewController {
         Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: {(authResult, error) in
             if let user = authResult?.user {
                 print(user)
+                
+                let uid = user.uid
+                var profileImageUrl: String?
+                
+                let storageRef = Storage.storage().reference(forURL: "gs://instagramcht-50ea5.appspot.com").child("profile_image").child(uid)
+                if let profileImg = self.selectedImage, let imageData = UIImageJPEGRepresentation(profileImg, 0.1){
+                    storageRef.putData(imageData, metadata: nil, completion: {(metadata, error) in
+                        if error != nil {
+                            return
+                        }
+                      //let profileImageUrl = metadata?.downloadURL().absoluteString
+                        storageRef.downloadURL(completion: { (url, error) in
+                            if error != nil {
+                                print("Failed to download url:", error!)
+                                return
+                            } else {
+                                //Do something with url
+                                profileImageUrl = url?.absoluteString
+                            }
+                            
+                        })
+                        
+                    })
+                }
+                
+                
+                
+                
                 self.ref = Database.database().reference()
                 let userReference = self.ref.child("users")
                 //print(userReference.description()) : https://instagramcht-50ea5.firebaseio.com
-                let uid = user.uid
+                //let uid = user.uid
                 let newUserReference = userReference.child(uid)
-                newUserReference.setValue(["username":self.usernameTextField.text!, "email": self.emailTextField.text!])
-                //print(" description: \(newUserReference.description())")
+                newUserReference.setValue(["username":self.usernameTextField.text!, "email": self.emailTextField.text!, "profileImageUrl": profileImageUrl])
+                print(" description: \(newUserReference.description())")
                 
                 
             
@@ -94,5 +135,16 @@ class SingleUpViewController: UIViewController {
         })
         
         
+    }
+}
+
+extension SingleUpViewController:UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("did Finish Picking Media")
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            selectedImage = image
+            profileImage.image = image
+        }
+        dismiss(animated: true, completion: nil)
     }
 }
